c8b137b5c8e4c1ffdc3848e62209239acc58dc43
NemanjaVaske <nemanjavasic49@yahoo.com>
2022-03-07 12:04:30 +0100

    Wheather for input city, and work with error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let cityName = "Belgrade";
+let cityName = "";
 let apiKey = "6060861c92d89cadc05f1c2ddf69c102";
+const form = document.getElementsByClassName("form")[0];
 
 const weatherApp = async () => {
   //jedinice su metric
@@ -8,11 +9,20 @@ const weatherApp = async () => {
       `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`,
       { mode: "cors" }
     );
-    const result = await data.json();
-    console.log(result);
+    if (data.status >= 200 && data.status <= 299) {
+      const result = await data.json();
+      console.log(result);
+    } else {
+      console.log("There is no such city");
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
-weatherApp();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  cityName = document.getElementById("location").value;
+  form.reset();
+  weatherApp();
+});
