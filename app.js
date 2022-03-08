d9b0a52d1a5f245cb51d45e29aed4a5a27cf80ad
Nemanja Vasic <86221451+NemanjaVaske@users.noreply.github.com>
2022-03-08 08:51:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ let apiKey = "6060861c92d89cadc05f1c2ddf69c102";
 const form = document.getElementsByClassName("form")[0];
 
 const weatherApp = async () => {
+  const weatherCity = document.getElementById("weather-city");
+  const weatherDesc = document.getElementById("weather-desc");
+  const weatherTemp = document.getElementById("weather-temp");
+  const weatherWind = document.getElementById("weather-wind");
   //jedinice su metric
   try {
     const data = await fetch(
@@ -12,10 +16,7 @@ const weatherApp = async () => {
     if (data.status >= 200 && data.status <= 299) {
       const { main, wind, weather } = await data.json();
 
-      const weatherCity = document.getElementById("weather-city");
-      const weatherDesc = document.getElementById("weather-desc");
-      const weatherTemp = document.getElementById("weather-temp");
-      const weatherWind = document.getElementById("weather-wind");
+      
 
       weatherCity.textContent = `Here is weather for ${cityName}`;
       weatherDesc.textContent = `${weather[0].description} `;
@@ -26,6 +27,9 @@ const weatherApp = async () => {
     } else {
       const weatherCity = document.getElementById("weather-city");
       weatherCity.textContent = "That city don't exist, search again!";
+      weatherDesc.textContent ='';
+      weatherTemp.textContent ='';
+      weatherWind.textContent ='';
     }
   } catch (error) {
     console.log(error);
